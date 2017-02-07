450765b106e0256b5561758ef3c626a219891147
kinshu200 <kinshu200@gmail.com>
2017-02-07 15:57:39 +0530

    My Commit Message

--- app/src/main/java/ajates/pro/astudios/com/youclip/StartupScreen.java
+++ app/src/main/java/ajates/pro/astudios/com/youclip/StartupScreen.java
@@ -1,11 +1,16 @@
 package ajates.pro.astudios.com.youclip;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
@@ -19,16 +24,22 @@ public class StartupScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup_screen);
       // getSupportActionBar().hide();
-        ImageView SplashView = (ImageView) findViewById(R.id.imageView);
-        Animation scaleAnimation = new ScaleAnimation(0f,1f,0f,1f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        scaleAnimation.setDuration(1000);
-        Animation SlideUpanimation = new TranslateAnimation(0,0,500,0);
-        SlideUpanimation.setDuration(1000);
-        AnimationSet animSet = new AnimationSet(true);
-        animSet.setFillEnabled(true);
-        animSet.addAnimation(scaleAnimation);
-        animSet.addAnimation(SlideUpanimation);
-        SplashView.startAnimation(animSet);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                //start your activity here
+                Intent i  =  new Intent(StartupScreen.this,DemoActivity.class);
+                startActivity(i);
+
+            }
+
+        }, 3000L);
 
     }
 
